4656ba1bafe0636aac4cd71995de355c726b931b
ismilearefin <arefinismile@gmail.com>
2022-10-24 21:52:09 +0600

    all course added

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const cors = require('cors');
 app.use(cors());
 
 const Port = process.env.Port || 5000;
+const allcourse = require("./Data/course.json");
 
 app.get('/', (res, req)=>{
-    req.send("Good Now You can create your own server")
+    req.send("Good Now You can create your own server");
 })
 
+app.get('/allcourse', (res,req)=>{
+    req.send(allcourse);
+})
 
 app.listen(Port, () => {
     console.log("server is running", Port);
