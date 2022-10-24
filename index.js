a95f43b544f10a9cd6b4040daba120379053120d
ismilearefin <arefinismile@gmail.com>
2022-10-24 21:48:17 +0600

    checked  my server

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+const Port = process.env.Port || 5000;
+
+app.get('/', (res, req)=>{
+    req.send("Good Now You can create your own server")
+})
+
+
+app.listen(Port, () => {
+    console.log("server is running", Port);
+});
+
 
 
